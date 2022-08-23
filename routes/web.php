774e12cde277b4cc8ea40f98b28839a6c93ac146
Sutthiphong <johndoe@example.com>
2022-08-23 16:17:53 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BackendController;
 
-Route::get('/',[HomeController::class,'home']);
-Route::get('about',[HomeController::class,'about']);
-Route::get('service',[HomeController::class,'service']);
-Route::get('contact',[HomeController::class,'contact']);
-Route::get('login',[HomeController::class,'login']);
+Route::get('/', [HomeController::class, 'home']);
+Route::get('about', [HomeController::class, 'about']);
+Route::get('service', [HomeController::class, 'service']);
+Route::get('contact', [HomeController::class, 'contact']);
+Route::get('login', [HomeController::class, 'login']);
+
+// Backend Route
+Route::get('backend/dashboard', [BackendController::class, 'dashboard']);
