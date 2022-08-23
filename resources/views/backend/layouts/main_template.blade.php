b02ba938db44477ac66ab0b6b7dd51659f95d039
Sutthiphong <johndoe@example.com>
2022-08-23 17:03:12 +0700

    update dashboard

--- resources/views/backend/layouts/main_template.blade.php
+++ resources/views/backend/layouts/main_template.blade.php
@@ -7,20 +7,20 @@
 
 <div class="wrapper">
     <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+        <img class="animation__shake" src="{{asset('assets/backend/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
 </div>
     @include('backend.includes.navbar')
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 <!-- Brand Logo -->
   <a href="index3.html" class="brand-link">
-    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <img src="{{asset('assets/backend/img/uttnfe.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">UTT NFE Admin</span>
   </a>
 
         @include('backend.includes.sidebar') 
 </aside>
-<div class="content-wrapper">
+<div class="content-wrapper" style="overflow: auto">
     @yield('content')
 </div>
 
